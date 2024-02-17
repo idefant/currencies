da00f85f9ceea97a83ab410db6d406487bdfcc8c
idefant <inik3004@gmail.com>
2024-02-17 21:53:49 +0500

    configure vite, eslint, prettier, docker, typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    sourcemap: 'hidden',
+  },
+  resolve: {
+    alias: {
+      '#': path.resolve(__dirname, './src'),
+      '#api': path.resolve(__dirname, './src/api'),
+      '#components': path.resolve(__dirname, './src/components'),
+      '#data': path.resolve(__dirname, './src/data'),
+      '#hooks': path.resolve(__dirname, './src/hooks'),
+      '#pages': path.resolve(__dirname, './src/pages'),
+      '#store': path.resolve(__dirname, './src/store'),
+      '#svg': path.resolve(__dirname, './src/svg'),
+      '#types': path.resolve(__dirname, './src/types'),
+      '#ui': path.resolve(__dirname, './src/ui'),
+      '#utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      enableBuild: false,
+      eslint: {
+        lintCommand: 'eslint -c .eslintrc.json --ext .js,.jsx,.ts,.tsx src',
+        dev: {
+          logLevel: ['error'],
+        },
+      },
+    }),
+  ],
+});
